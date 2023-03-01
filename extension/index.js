a516a45fe01ea531ebf5e8d5f04230c79c6dc74f
洛七 <91249019+Onlyn1ght@users.noreply.github.com>
2023-03-01 10:59:47 +0800

    解决载入用户信息失败
    maybe

--- extension/index.js
+++ extension/index.js
@@ -1,38 +1,41 @@
-/* eslint-disable no-unused-vars */
 const path = require('path');
 
 function activate(context, electron) {
-    context.on('login', function(token) {
-        console.dir(token);
-        login(context);
-    })
-
-    context.on('logout', () => {
-        console.dir('user logout')
-    })
-
-    context.on('quit', () => {
-        console.dir('app was quit')
-    })
-
-    context.on('command', async (command, args, callback) => {
-        switch(command) {
-            case 'fishpi.info.get':
-            {
-                let rsp = await context.fishpi.account.info();
-                console.dir(rsp)
-                callback(rsp.data);
-                break;
-            }
-        }
-    })
-}
-
-function login(context) {
+  function setSidebar() {
     let iconPath = path.join(__dirname, "..", "public", "icon.svg");
-    let Url = process.env.EXT_ENV == 'development' ? 
-        "http://127.0.0.1:8080" :
-        path.join(__dirname, "..", "dist", "index.html");
+    let Url = process.env.EXT_ENV == 'development' ?
+      "http://127.0.0.1:8080" :
+      path.join(__dirname, "..", "dist", "index.html");
     context.setSidebar(iconPath, Url);
+  }
+
+  // Set the sidebar on activation
+  setSidebar();
+
+  context.on('login', function(token) {
+    console.dir(token);
+    setSidebar();
+  })
+
+  context.on('logout', () => {
+    console.dir('user logout')
+  })
+
+  context.on('quit', () => {
+    console.dir('app was quit')
+  })
+
+  context.on('command', async (command, args, callback) => {
+    switch(command) {
+      case 'fishpi.info.get':
+        {
+          let rsp = await context.fishpi.account.info();
+          console.dir(rsp)
+          callback(rsp.data);
+          break;
+        }
+    }
+  })
 }
+
 module.exports = { activate }
